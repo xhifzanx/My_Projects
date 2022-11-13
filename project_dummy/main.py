b005e90268af2731ae56217f2ecfb28e12531962
xhifzanx <misterj435@gmail.com>
2022-11-13 20:30:07 +0530

    updated

--- project_dummy/main.py
+++ project_dummy/main.py
@@ -5,5 +5,5 @@ password = "YOUR PASSWORD"
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="misterj435@gmail.com", msg="hello")
+connection.sendmail(from_addr=my_email, to_addrs="someemail", msg="hello")
 connection.close()
