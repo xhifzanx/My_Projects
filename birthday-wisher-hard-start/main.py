dd0d1f60b9396767ee85b77ce44070858e198002
xhifzanx <misterj435@gmail.com>
2022-11-13 19:27:23 +0530

    updated

--- birthday-wisher-hard-start/main.py
+++ birthday-wisher-hard-start/main.py
@@ -3,23 +3,7 @@ import pandas
 import datetime as dt
 import random
 import smtplib
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes.
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
 
 data = pandas.read_csv("birthdays.csv")
 birthdays_data = data.to_dict(orient="records")
@@ -40,8 +24,8 @@ for i in range(4):
             with open(location, "r") as data:
                 content = data.read()
                 new_content = content.replace("[NAME]", f"{birthdays_data[i]['name']}")
-            my_email = "dummyacounhotmail@gmail.com"
-            password = "hifzan1234"
+            my_email = "YOUREMAIL"
+            password = "YOURPASSWORD"
             connection = smtplib.SMTP("smtp.gmail.com")
             connection.starttls()
             connection.login(user=my_email, password=password)
